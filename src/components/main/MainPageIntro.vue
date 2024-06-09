828a7d818f2ea8f669eeb8009c872f0207e93f21
Weegoos <batyr.ashim05@mail.ru>
2024-06-10 01:53:31 +0500

    Batyr new

--- src/components/main/MainPageIntro.vue
+++ src/components/main/MainPageIntro.vue
@@ -2,7 +2,7 @@
   <div>
       <center>
         <div class="q-pt-md">
-        <p class="introtext" :class="[$q.screen.width < 1200 ? 'introTextMobile' : 'introTextDesktop']">Movie resources</p>
+        <p class="introtext q-pt-xl" :class="[$q.screen.width < 1200 ? 'introTextMobile' : 'introTextDesktop']">Movie resources</p>
         <p class="quoteText">A manual for joining the MovieSelect ecosystem. By builders for builders.</p>
         <q-btn label="Build now"  rounded flat class="introBtn" />
       </div>
@@ -17,5 +17,7 @@ export default {
 </script>
 
 <style>
-
+.introtext{
+  padding-top: 60px;
+}
 </style>
