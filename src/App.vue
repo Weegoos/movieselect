c460a6028e6a7a5a5ab706a7628d6468cb5ee70b
Weegoos <batyr.ashim05@mail.ru>
2024-06-10 00:51:56 +0500

    Batyr new

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,10 @@
 </template>
 
 <script setup>
+import { onBeforeMount } from 'vue';
 import headerPage from './pages/Header.vue';4
+
+onBeforeMount(() => {
+  console.clear()
+})
 </script>
--- src/pages/Header.vue
+++ src/pages/Header.vue
@@ -1,19 +1,19 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <q-layout 
-  view="lHh LpR lff"
+  view="hHh LpR lff"
   container 
   style="height: 70px"
-  class="shadow-2 rounded-borders"
+  class="header rounded-borders"
 
   >
-    <q-header class="bg-transparent text-black q-mt-sm q-pl-md">
-        <q-toolbar class="q-gutter-sm">
+    <q-header class="bg-black q-mt-sm q-pl-md header">
+        <q-toolbar class="q-gutter-sm" color="white">
           <q-btn color="black" icon="menu">
-        <q-menu>
-          <q-list style="min-width: 100px">
-            <q-item clickable v-close-popup>
-              <q-btn to="/" dense no-caps flat>Домой</q-btn>
+        <q-menu class="bg-black">
+          <q-list style="min-width: 100px;">
+            <q-item  v-close-popup clickable>
+              <q-btn to="/" class="text" dense no-caps flat >Домой</q-btn>
             </q-item>
             <q-item clickable>
                 <q-item-section>Поиск фильмов</q-item-section>
@@ -34,31 +34,30 @@
                 </q-menu>
 
               </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>New incognito tab</q-item-section>
+            <q-item clickable>
+              <q-item-section>Поиск сериалов</q-item-section>
+              <q-item-section side>
+                <q-icon name="keyboard_arrow_right"/>
+              </q-item-section>
+
+              <q-menu anchor="top end" self="top start">
+                  <q-list dense>
+                    <q-item
+                      v-for="film in menuFilms"
+                      :key="film.id"
+                      clickable
+                    >
+                      <q-btn dense flat no-caps :to="film.link">{{ film.type }}</q-btn>
+                    </q-item>
+                  </q-list>
+                </q-menu>
             </q-item>
             <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Recent tabs</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>History</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>Downloads</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Settings</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Help &amp; Feedback</q-item-section>
-            </q-item>
+           
           </q-list>
         </q-menu>
       </q-btn>
-            <div class="text-h5">
+            <div class="text-h5" style="color: white">
               Movie Select
           <q-badge outline align="middle" color="teal">
             v1.1.0
@@ -85,7 +84,7 @@ export default {
       },
       {
         id: 3,
-        type: 'По название'
+        type: 'По названию'
       }
     ]
   }
@@ -94,5 +93,6 @@ export default {
 </script>
 
 <style>
+
 
 </style>
